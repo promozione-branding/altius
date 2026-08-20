90a285cc856d19986575d6ea0a6ec45ae29d57aa
manish kumar <mk7219630@gmail.com>
2026-08-20 16:17:51 +0530

    about bg image

--- compoents/Home/About.jsx
+++ compoents/Home/About.jsx
@@ -803,7 +803,7 @@ export default function About() {
           "
           style={{
             backgroundImage:
-              "url('https://images.unsplash.com/photo-1600566753086-00f18fb6b3ea?auto=format&fit=crop&w=1800&q=90')",
+              "url('https://images.unsplash.com/photo-1772112334844-2eed0111e690?q=80&w=1094&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
           }}
         >
           {/* ================= OVERLAY ================= */}
