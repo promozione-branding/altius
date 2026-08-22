d337fef755c686ba4154b42bdb1544d75b6d7de3
manish kumar <mk7219630@gmail.com>
2026-08-22 18:32:18 +0530

    Home page Position Change

--- app/page.js
+++ app/page.js
@@ -1,37 +1,47 @@
-import React from 'react'
+import React from "react";
 // import Navbar from '../compoents/Home/Navbar'
-import Featured from '../compoents/Home/Featured'
-import About from '../compoents/Home/About'
+import Featured from "../compoents/Home/Featured";
+import About from "../compoents/Home/About";
 // import Footer from '../compoents/Home/Footer'
-import Hero from '../compoents/Home/Hero'
-import Collections from '../compoents/Home/Collections'
-import Faq from '../compoents/Home/Faq'
-import Form from '../compoents/Home/Form'
-import WhyChooseUs from '../compoents/Home/WhyChooseUs'
-import ShopByCategory from '../compoents/Home/ShopByCategory'
+import Hero from "../compoents/Home/Hero";
+import Collections from "../compoents/Home/Collections";
+import Faq from "../compoents/Home/Faq";
+import Form from "../compoents/Home/Form";
+import WhyChooseUs from "../compoents/Home/WhyChooseUs";
+import ShopByCategory from "../compoents/Home/ShopByCategory";
 // import CTA from '@/compoents/Home/CTA'
-import Testimonials from '@/compoents/Home/Testimonials'
-import CTA2 from '@/compoents/Home/CTA2'
-import Scroll from '@/compoents/Home/Scroll'
-import Collections2 from '@/compoents/Home/Collections2'
+import Testimonials from "@/compoents/Home/Testimonials";
+import CTA2 from "@/compoents/Home/CTA2";
+import Scroll from "@/compoents/Home/Scroll";
+import Collections2 from "@/compoents/Home/Collections2";
+import CurvedLoop from "@/compoents/ui/CurvedLoop";
+import Parllel from "@/compoents/Home/Parllel";
 
 export default function page() {
   return (
     <>
-    <Hero/>
-    <Featured/>
-    <About/>
-    
-    <ShopByCategory/>
-    <Scroll/>
-    <WhyChooseUs/>
-    {/* <Collections/> */}
-    <Collections2/>
-    <Form/>
-    <Faq/>
-    <Testimonials/>
-    {/* <CTA/> */}
-    <CTA2/>
+      <Hero />
+      <CurvedLoop
+        marqueeText="ELSTRONG ✦ BRIGHTER SPACES ✦ SMARTER LIGHTING ✦ LED INNOVATION ✦"
+        speed={100}
+        curveAmount={0}
+        direction="left"
+        interactive={true}
+      />
+      <About />
+      <Featured />
+      <Parllel/>
+
+      <ShopByCategory />
+      <Scroll />
+      <WhyChooseUs />
+      {/* <Collections/> */}
+      <Collections2 />
+      <Form />
+      <Faq />
+      <Testimonials />
+      {/* <CTA/> */}
+      <CTA2 />
     </>
-  )
+  );
 }
