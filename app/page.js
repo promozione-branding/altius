9d79b97add3b331d6a211b7b0441a3cdefcd9716
manish kumar <mk7219630@gmail.com>
2026-08-24 17:01:54 +0530

    marquee

--- app/page.js
+++ app/page.js
@@ -21,13 +21,13 @@ export default function page() {
   return (
     <>
       <Hero />
-      {/* <CurvedLoop
+      <CurvedLoop
         marqueeText="ELSTRONG ✦ BRIGHTER SPACES ✦ SMARTER LIGHTING ✦ LED INNOVATION ✦"
         speed={100}
         curveAmount={0}
         direction="left"
         interactive={true}
-      /> */}
+      />
       <About />
       <Featured />
       <Parllel />
